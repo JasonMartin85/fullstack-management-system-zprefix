4defa345a9841ef684b4e95afd2b995d3d1fc18b
JasonMartin85 <jason.m.martin85@gmail.com>
2023-04-05 23:48:28 -0400

    Added tailwind.css and flowbite react to project for styling

--- front-end/src/Home/Home.js
+++ front-end/src/Home/Home.js
@@ -1,38 +1,59 @@
-import React, {useEffect, useState} from 'react'
-import {useNavigate} from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Table } from "flowbite-react";
 
 const Home = () => {
-  const [itemsArray, setItemsArray] = useState()
-  const navigate = useNavigate()
+  const [itemsArray, setItemsArray] = useState();
+  const navigate = useNavigate();
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch(`http://localhost:3001/items`)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-      setItemsArray(data)
-      })
-  },[])
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setItemsArray(data);
+      });
+  }, []);
 
   const itemClick = (id) => {
-    navigate(`/item/${id}`)
-  }
+    navigate(`/item/${id}`);
+  };
 
-  return(<>
-    <h1>Home</h1>
-    <ul>
-      {itemsArray 
-        ? <div>{itemsArray.map(item => 
-            <div  
-              key={item.id} 
-              onClick={()=>{itemClick(item.id)
-            }}>
-                {item.item_name} {item.description ? item.description.slice(0,100) : <div></div>}
-            </div>)}
-          </div> 
-        : <div>Loading</div>}
-    </ul>
-  </>)
-}
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Home</h1>
+
+      <table class="table-fixed">
+        <thread>
+          <tr>
+            <th className="width-1">ID</th>
+            <th className="width-52">Name</th>
+            <th className="width-96">Description</th>
+          </tr>
+        </thread>
+        <tbody>
+        {itemsArray ? (
+          itemsArray.map((item) => {
+            return (
+              
+              <tr>
+              <td>{item.id}</td>
+              <td>{item.item_name}</td>
+              <td>{item.description ? item.description.slice(0,100) : <></>}</td>
+              </tr>
+            );
+          })
+        ) : (
+          <div>Loading</div>
+        )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Home;
+
+// onClick={()=>{itemClick(item.id)}
+//
+
